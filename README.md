fe75ca5674c72ef9c72369ba25cd7ceca2c5d5b5
Garry Yao <yaojun85@gmail.com>
2017-12-16 00:26:28 +0000

    auto importing locale files for select2 by hooking into "included"

--- README.md
+++ README.md
@@ -1,26 +1,43 @@
-# ember-cli-select2
+# ember-cli-shim-select2
 
-This README outlines the details of collaborating on this Ember addon.
+Ember CLI add-on helps to load [select2](https://github.com/select2/select2) locales in your Ember app, with a smaller footprint.
 
-## Installation
+## Usage
 
-* `git clone <repository-url>` this repository
-* `cd ember-cli-select2`
-* `npm install`
+`bower install --save select2`
+`ember install ember-cli-shim-select2`
 
-## Running
+Add locales preferences to your app's config file:
+```
+// config.environment.js
+module.exports = function(environment) {
+  return {
+    // ...
 
-* `ember serve`
-* Visit your app at [http://localhost:4200](http://localhost:4200).
+    select2: {
+      // To cherry-pick specific locale support into your application.
+      // Full list of locales:
+      includeLocales: ['es', 'zh-cn']
+    }
+  };
+```
+
+You should be able to specify one locale when creating a select2 instance:
+
+```
+$(".js-example-language").select2({
+  language: "es"
+});
+
+```
+
+
+NOTE: English is the default language, so adding 'en-*' to the locale list will not actually doing anything.
+
+When a specific locale file fails, the add-on will try to locate a more generic locale, suppose that you specify `zh-cn` on the list but no locale file for that in select2 package, a `zh` locale will be used. If still found no locale in the package a warning will be displayed from the build output.
 
 ## Running Tests
 
 * `npm test` (Runs `ember try:each` to test your addon against multiple Ember versions)
 * `ember test`
 * `ember test --server`
-
-## Building
-
-* `ember build`
-
-For more information on using ember-cli, visit [https://ember-cli.com/](https://ember-cli.com/).
--- index.js
+++ index.js
@@ -1,6 +1,76 @@
 /* eslint-env node */
 'use strict';
+const existsSync = require('exists-sync');
+const chalk = require('chalk');
+const path = require('path');
+
+const packageName = 'select2';
 
 module.exports = {
-  name: 'ember-cli-select2'
+  name: 'ember-cli-shim-select2',
+  included(app) {
+    this._super.included.apply(this, arguments);
+    let options = this.getOptions();
+    const locales = options.includeLocales;
+
+    // Assuming select2 installed as bower dependency
+    // TODO:
+    const pkgPath = path.join(app.bowerDirectory, packageName);
+    if (locales) {
+      this.import(
+        {
+          development: `${pkgPath}/select2.css`,
+          production: `${pkgPath}/select2.min.css`
+        }
+      );
+      this.import(
+        {
+          development: `${pkgPath}/select2.js`,
+          production: `${pkgPath}/select2.min.js`
+        }
+      );
+
+      if (Array.isArray(locales)) {
+        locales.forEach(locale => {
+          this.import(`${pkgPath}/select2_locale_${locale}.js`);
+        });
+      }
+    }
+  },
+  getOptions() {
+    const config = this.project.config(process.env.EMBER_ENV)[packageName] || {};
+    if (Array.isArray(config.includeLocales)) {
+
+      // Filter down the locales to only those are actually exists in the package.
+      config.includeLocales = config.includeLocales
+      .filter(locale => typeof locale === 'string')
+      .map(locale => locale.replace('.js', '').trim().toLowerCase())
+      .filter(locale => {
+
+        if (!this.checkLocaleExists(locale)) {
+          // Fallback once to the generic locale if this particular is not found in place.
+          locale = locale.replace(/-\w+$/, '');
+        }
+        if (locale === 'en') {
+          // `en` is included by default.  quietly ignore if user specifies it in the list
+          return false;
+        }
+        if (!this.checkLocaleExists(locale)) {
+          this.ui.writeLine(
+            chalk.red(
+              `ember-cli-shim-select2: Specified locale "${locale}" but could not find in select2.\n
+              Visit https://select2.org/i18n to view the full list of supported locales.`
+            )
+          );
+          return false;
+        }
+        return true;
+      });
+    }
+    return config;
+  },
+  checkLocaleExists(locale) {
+    const location = path.join(this.app.project.root, `${this.app.bowerDirectory}`, packageName);
+    return existsSync(path.join(location, `select2_locale_${locale}.js`))
+  }
 };
